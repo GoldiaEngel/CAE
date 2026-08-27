a2b635ba9ed043bdcb4e34f2da768734936cf7da
GoldiaEngel <tqsboss@gmail.com>
2026-08-27 10:06:01 +0700

    Add files via upload

---
+++ data/cae_listening/test3/config.js
@@ -0,0 +1,11 @@
+window.EXAM_CONFIG = {
+    examName: "CAE Listening - Test 1",
+    isListening: true,
+    totalParts: 4, 
+    parts: {
+        1: { name: "Part 1", file: "part1.js", total: 6, range: [1, 2, 3, 4, 5, 6] },
+        2: { name: "Part 2", file: "part2.js", total: 8, range: [7, 8, 9, 10, 11, 12, 13, 14] },
+        3: { name: "Part 3", file: "part3.js", total: 6, range: [15, 16, 17, 18, 19, 20] },
+        4: { name: "Part 4", file: "part4.js", total: 10, range: [21, 22, 23, 24, 25, 26, 27, 28, 29, 30] }
+    }
+};
---
+++ data/cae_listening/test3/part1.js
@@ -0,0 +1,85 @@
+window.PART_DATA_1 = {
+    title: "Part 1 - Multiple Choice",
+    audio: "CAE/data/cae_listening/test1/part1.mp3",
+    instructions: "You will hear three different extracts. For questions 1–6, choose the answer (A, B, or C) which fits best according to what you hear. There are two questions for each extract.",
+    extracts: [
+        {
+            id: 1,
+            context: "Extract One: You will hear two people talking about a play they saw at the theatre.",
+            questions: [
+                {
+                    number: 1,
+                    prompt: "What do we learn about the writer of the play?",
+                    options: [
+                        "He died.",
+                        "He is dying.",
+                        "He nearly died."
+                    ],
+                    answer: 2
+                },
+                {
+                    number: 2,
+                    prompt: "What do the speakers agree on?",
+                    options: [
+                        "The writer is quite predictable.",
+                        "The writer's style has changed direction.",
+                        "The play wasn't as funny as some of the writer's earlier plays."
+                    ],
+                    answer: 1
+                }
+            ]
+        },
+        {
+            id: 2,
+            context: "Extract Two: You will hear part of a radio interview with a trade and commerce researcher.",
+            questions: [
+                {
+                    number: 3,
+                    prompt: "According to Pablo Jenson",
+                    options: [
+                        "a variety of different shops tends to boost sales.",
+                        "traders that have something in common can boost each other's sales.",
+                        "butchers are the most successful traders."
+                    ],
+                    answer: 1
+                },
+                {
+                    number: 4,
+                    prompt: "Jenson's theory",
+                    options: [
+                        "advertised for similar retailers to open shops in one particular area.",
+                        "works better for bakers and butchers than for other kinds of retailers.",
+                        "seems to have been proved to be credible."
+                    ],
+                    answer: 2
+                }
+            ]
+        },
+        {
+            id: 3,
+            context: "Extract Three: You will hear a report about holiday homes in the Mediterranean.",
+            questions: [
+                {
+                    number: 5,
+                    prompt: "The Costa de la Cruz",
+                    options: [
+                        "is being spoiled by developers.",
+                        "is the cheapest area of Spain in which you can buy a holiday home.",
+                        "is close to Portugal."
+                    ],
+                    answer: 2
+                },
+                {
+                    number: 6,
+                    prompt: "According to Chris Mercer",
+                    options: [
+                        "there is a danger that the Costa de la Cruz will lose its appeal.",
+                        "the Costa de la Cruz can be developed without the area being damaged.",
+                        "the government has put a stop to the Costa de la Cruz being developed further."
+                    ],
+                    answer: 1
+                }
+            ]
+        }
+    ]
+};
